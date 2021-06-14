8b7af6df39bdfc257ddb1f6c683d3f95739666f5
Austregíselo Junior <austregiselosvj@hotmail.com>
2021-06-14 20:45:26 -0300

    Adicionando curso de Asp.Net Core básico

---
+++ AspNet Core/Webapp com padrão MVC/Alura.ListaLeitura/Alura.ListaLeitura.App/Views/HtmlLUtios.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Alura.ListaLeitura.App.HTML
+{
+  public  class HtmlLUtios
+    {
+        public static string CarregandoArquivoHTML(string nomedoArquivo)
+        {
+            using (var arquivo = File.OpenText($"HTML/{nomedoArquivo}.html"))
+            {
+                return arquivo.ReadToEnd();
+            }
+        }
+    }
+}
