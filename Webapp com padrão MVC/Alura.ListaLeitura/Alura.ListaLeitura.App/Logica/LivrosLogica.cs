825ff252af8bf2b981fceb01fc3caece9d4078e2
Austregíselo Junior <austregiselosvj@hotmail.com>
2021-06-16 10:05:18 -0300

    Add projeto final do curso.

---
+++ Webapp com padrão MVC/Alura.ListaLeitura/Alura.ListaLeitura.App/Logica/LivrosLogica.cs
@@ -0,0 +1,65 @@
+﻿using Alura.ListaLeitura.App.HTML;
+using Alura.ListaLeitura.App.Negocio;
+using Alura.ListaLeitura.App.Repositorio;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alura.ListaLeitura.App.Logica
+{
+    public class LivrosController : Controller
+    {
+
+
+        public string CarregaLista(IEnumerable<Livro> livros)
+        {
+            var conteudoArquivo = HtmlLUtios.CarregandoArquivoHTML("Lista");
+            foreach (var item in livros)
+            {
+                conteudoArquivo = conteudoArquivo
+                    .Replace("#NOVO-ITEM#", $"<li>{item.Titulo} - {item.Autor}</li>#NOVO-ITEM#");
+            }
+            return conteudoArquivo.Replace("#NOVO-ITEM#", "");
+        }
+
+
+        public static Task ExibirDestalhes(HttpContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Detalhes(int id)
+        {
+            var repo = new LivroRepositorioCSV();
+            var livro = repo.Todos.First(x => x.Id == id);
+            return livro.Detalhes();
+        }
+
+        public IActionResult ParaLer()
+        {
+            var _repo = new LivroRepositorioCSV();
+            ViewBag.Livros = _repo.ParaLer.Livros;
+            return View("Lista");
+        }
+
+        public IActionResult Lidos()
+        {
+            var _repo = new LivroRepositorioCSV();
+            ViewBag.Livros = _repo.Lidos.Livros;
+            return View("lista");
+        }
+
+        public IActionResult Lendo()
+        {
+            var _repo = new LivroRepositorioCSV();
+            ViewBag.Livros = _repo.Lendo.Livros;
+            return View("lista");
+        }
+    }
+}
